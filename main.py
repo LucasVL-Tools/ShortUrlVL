d809ca76b622f2e561cb4b4827007dc46eaa58a1
LucasVL <lucas@lucasvl.nl>
2022-10-31 13:05:55 -0500

    cleaned up shitty css

--- main.py
+++ main.py
@@ -274,9 +274,14 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 # checks for unix socket or host in config file and runs waitress accordingly. 
-#app.run(debug = True)
+
+from werkzeug.middleware.profiler import ProfilerMiddleware
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[5], profile_dir="./profile")
+app.run(debug = True)
+"""
 if host:
     serve(app, host=host, port=port)
 elif unix_socket:
     serve(app, unix_socket=unix_socket, unix_socket_perms="777")
 else: print("Please specify a host or unix socket (you probably just want host to be set to 0.0.0.0)")
+"""
